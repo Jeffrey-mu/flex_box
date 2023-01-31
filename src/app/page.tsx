a09059c5b3036396564b17518bc170e03fd477bc
Jeffrey <1115378579@qq.com>
2023-01-31 15:01:00 +0800

    init: first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,227 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client"
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
-const inter = Inter({ subsets: ['latin'] })
+import Header from "../components/header"
+const directions = [
+  { id: 1, name: 'row', value: 'flex-row' },
+  { id: 2, name: 'row-reverse', value: 'flex-row-reverse' },
+  { id: 3, name: 'column', value: 'flex-col' },
+  { id: 4, name: 'column-reverse', value: 'flex-col-reverse' },
+]
 
-export default function Home() {
+const justifyContents = [
+  { id: 1, name: 'flex-start', value: 'justify-start' },
+  { id: 2, name: 'flex-end', value: 'justify-end' },
+  { id: 3, name: 'center', value: 'justify-center' },
+  { id: 4, name: 'space-between', value: 'justify-between' },
+  { id: 4, name: 'space-around', value: 'justify-around' },
+  { id: 4, name: 'space-evenly', value: 'justify-evenly' },
+]
+
+const wraps = [
+  { id: 1, name: 'wrap', value: 'flex-wrap' },
+  { id: 2, name: 'wrap-reverse', value: 'flex-wrap-reverse' },
+  { id: 3, name: 'nowrap', value: 'flex-nowrap' },
+]
+export default function Example() {
+  const [direction, setDirection] = useState(directions[0])
+  const [justifyContent, setjustifyContent] = useState(justifyContents[0])
+  const [wrap, setWrap] = useState(wraps[0])
+  const [wrapState, setSrapState] = useState(false)
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className='max-w-5xl mx-auto min-h-screen'>
+        <Header />
+        <div className="mt-5">
+          <div className='flex'>
+            <Listbox className="w-[200px]" value={direction} onChange={(e) => { setSrapState(false); setDirection(e) }}>
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                  <span className="block truncate">{direction.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {directions.map((direction, directionIdx) => (
+                      <Listbox.Option
+                        key={directionIdx}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                          }`
+                        }
+                        value={direction}
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                }`}
+                            >
+                              {direction.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+            <Listbox className="w-[200px] ml-2" value={justifyContent} onChange={(e) => { setSrapState(false); setjustifyContent(e) }} >
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                  <span className="block truncate">{justifyContent.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {justifyContents.map((justifyContent, justifyContentIdx) => (
+                      <Listbox.Option
+                        key={justifyContentIdx}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                          }`
+                        }
+                        value={justifyContent}
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${justifyContent ? 'font-medium' : 'font-normal'
+                                }`}
+                            >
+                              {justifyContent.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+            <Listbox className="w-[200px] ml-2" value={wrap} onChange={(e) => { setSrapState(true); setWrap(e) }}>
+              <div className="relative mt-1">
+                <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                  <span className="block truncate">{wrap.name}</span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+                <Transition
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                    {wraps.map((wrap, wrapIdx) => (
+                      <Listbox.Option
+                        key={wrapIdx}
+                        className={({ active }) =>
+                          `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                          }`
+                        }
+                        value={wrap}
+                      >
+                        {({ selected }) => (
+                          <>
+                            <span
+                              className={`block truncate ${wrap ? 'font-medium' : 'font-normal'
+                                }`}
+                            >
+                              {wrap.name}
+                            </span>
+                            {selected ? (
+                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </Listbox>
+          </div>
+          <div className={`flex border-2 mt-5 flex-1 w-[500px] h-[500px] rounded-md ${direction.value} ${justifyContent.value} ${wrap.value}`}>
+            <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+              <span className='text-white '>
+                1
+              </span>
+            </div>
+            <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+              <span className='text-white '>
+                2
+              </span>
+            </div>
+            <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+              <span className='text-white '>
+                3
+              </span>
+            </div>
+            {
+              (wrapState && <>
+                <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+                  <span className='text-white '>
+                    4
+                  </span>
+                </div>
+                <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+                  <span className='text-white '>
+                    5
+                  </span>
+                </div>
+                <div className='w-1/6 h-1/6 bg-current m-2 rounded-md'>
+                  <span className='text-white '>
+                    6
+                  </span>
+                </div>
+              </>)
+            }
+          </div>
+
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
