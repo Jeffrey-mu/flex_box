f6fd43f4af5ac35a8dc6b0c7608d57928042b10e
Jeffrey <1115378579@qq.com>
2023-09-04 15:31:02 +0800

    feat: 添加ads脚本

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,8 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="flex box css html" />
       <link rel="icon" href="/favicon.ico" />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6578177460190720"
+        crossorigin="anonymous"></script>
     </>
   )
 }
